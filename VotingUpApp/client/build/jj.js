b2ab934049bcf043a4735642ef2403b84197535e
darkaskamandas <63114241+darkaskamandas@users.noreply.github.com>
2022-03-05 01:14:57 +0200

    Add files via upload

---
+++ VotingUpApp/client/build/jj.js
@@ -0,0 +1,11 @@
+function createChart() {
+    var data = [{
+            language: 'JavaScript',
+            rating: 10
+        },
+        {
+            language: 'Css',
+            rating: 10
+        }
+    ]
+}
